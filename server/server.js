f7296625d18252369362badfaa29ac3bfd9246e0
LOGESH M <stylishtamil25@gmail.com>
2026-03-27 09:23:45 +0530

    edit server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
+
+process.on("unhandledRejection", (reason) => {
+  console.error("Unhandled Promise Rejection:", reason);
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+  process.exit(1);
+});
 const expandLocalOrigins = (origins) => {
   const expanded = new Set(origins);
   for (const origin of origins) {
@@ -37,6 +47,7 @@ io.on("connection", (socket) => {
 });
 
 const start = async () => {
+  console.log("Starting server...");
   await connectDB();
   server.listen(PORT, () => console.log(`Server running on ${PORT}`));
 };
